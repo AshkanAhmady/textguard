02b4040556151a1d508f6632f82e530ab2e319c9
Ashkan <ashkan16ma96@gmail.com>
2026-06-12 18:07:54 +0330

    feat(en): add leetspeak character mapping dictionary

--- packages/en/src/index.ts
+++ packages/en/src/index.ts
@@ -2,3 +2,4 @@ export { enProfanity } from "./profanity";
 export { enInsults } from "./insults";
 export { enSpam } from "./spam";
 export { enPatterns } from "./patterns";
+export { enLeetspeakMapping } from "./leetspeak";
