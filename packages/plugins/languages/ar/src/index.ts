467e1c9cb87984b7bc15b07573d88c1be59a1812
Ashkan Ahmady <ashkan16ma96@gmail.com>
2026-08-09 17:09:00 +0330

    feat(ar): expose usable base dictionary

--- packages/plugins/languages/ar/src/index.ts
+++ packages/plugins/languages/ar/src/index.ts
@@ -1,13 +1,22 @@
 import type { Dictionary } from "@textguard/core";
 
+import { arInsults } from "./insults";
+import { arProfanity } from "./profanity";
+
+export { arProfanity };
+export { arInsults };
+
+export const arPack = {
+  profanity: arProfanity,
+  insults: arInsults,
+};
+
 export const arDictionary: Dictionary = {
   name: "ar",
   language: "ar",
-  version: "0.0.0",
-  words: [],
+  version: "1.0.0",
+  words: [...arProfanity.words, ...arInsults.words],
 };
-
-export const arPack = {};
 
 export const arLanguage = {
   code: "ar",
