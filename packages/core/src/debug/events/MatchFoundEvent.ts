859d14ec2a0a0cc09445da7bf757111a90ccbac9
Ashkan Ahmady <ashkan16ma96@gmail.com>
2026-08-09 11:55:38 +0330

    feat(core): attach rule metadata to match events

--- packages/core/src/debug/events/MatchFoundEvent.ts
+++ packages/core/src/debug/events/MatchFoundEvent.ts
@@ -1,11 +1,13 @@
 import type { BaseDebugEvent } from "./BaseDebugEvent";
 import type { Match } from "../../domain/match";
+import type { DebugRuleMetadata } from "../models/DebugRuleMetadata";
 
 export interface MatchFoundEvent extends BaseDebugEvent {
   readonly type: "match:found";
 
   readonly plugin: string;
   readonly rule: string;
+  readonly ruleMetadata: DebugRuleMetadata;
 
   readonly match: Match;
 }
