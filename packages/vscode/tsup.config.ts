09eab21a71362f4c79879a98908a8a23a5fccf20
Ashkan Ahmady <ashkan16ma96@gmail.com>
2026-08-12 02:15:36 +0330

    fix(vscode): avoid workspace lockfile drift

--- packages/vscode/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/extension.ts"],
-  format: ["cjs"],
-  platform: "node",
-  external: ["vscode"],
-  clean: true,
-});
