c33107a733de294b36e231c3a5998513c574bdbe
Ashkan Ahmady <ashkan16ma96@gmail.com>
2026-08-14 02:38:58 +0330

    fix(all): explicitly export detector factories

--- packages/all/src/index.ts
+++ packages/all/src/index.ts
@@ -1,3 +1,12 @@
 export * from "@textguard/core";
 export * from "./plugins";
+export {
+  emailPlugin,
+  urlPlugin,
+  phonePlugin,
+  ipPlugin,
+  uuidPlugin,
+  creditCardPlugin,
+  ibanPlugin,
+} from "./plugins";
 export * from "./presets";
